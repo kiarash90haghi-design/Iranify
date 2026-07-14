196a5f8353d790c404d7265c4edeba68557d9d02
kiarash <kiarash90haghi@gmail.com>
2026-07-14 18:09:36 +0330

    Create initial layout for Iranify music player

---
+++ assets/js/app.js
@@ -0,0 +1,12 @@
+const albums = [
+  {
+    title: "Blinding Lights",
+    artist: "The Weeknd",
+    cover: "cover1.jpg"
+  },
+  {
+    title: "Starboy",
+    artist: "The Weeknd",
+    cover: "cover2.jpg"
+  }
+];
